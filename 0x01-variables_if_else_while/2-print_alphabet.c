66861d46c7e9e07ead35abfb6216bac24200c2e7
MUTEGEKI DAVID <mutegeki.david555@gmail.com>
2022-04-10 00:12:37 -0700

    my commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (success/correct)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while ( ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
+

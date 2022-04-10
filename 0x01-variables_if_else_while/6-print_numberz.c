7dc4a43f8ad28a8ea3b446417c383a537ffa1231
MUTEGEKI DAVID <mutegeki.david555@gmail.com>
2022-04-10 03:23:00 -0700

    my commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+/** main -Entry point
+ *
+ * Return : Always 0 (success/correct)
+ */
+int main(void)
+{
+	putchar('0'+'1'+'2'+'3'+'4'+'5');
+	putchar('6'+'7'+'8'+'9');
+
+        return (0);
+}
+
+
+

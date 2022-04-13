71abbb2d3677638c358ebe910433b457e542205f
MUTEGEKI DAVID <mutegeki.david555@gmail.com>
2022-04-13 05:24:29 -0700

    my commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+:#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints the alphabet 10 times
+ *
+ * return :nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+	int n,i;
+
+	i=0;
+
+	while (i < 10)
+	{
+		for(n = 'a' ; n <= 'z' ;n++)
+		{
+			_putchar(n);
+		}
+		i++;
+	        _putchar('\n');
+	}
+
+
+

8b99c864dc8157471e377fd0d6ba8d18035ebf42
MUTEGEKI DAVID <mutegeki.david555@gmail.com>
2022-04-13 10:25:50 -0700

    my commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit -prints the last digit in a number
+ * @n:int type number
+ * Return: the last digit
+ */
+
+int print_last_digit(int c)
+{
+	int nv;
+
+	if (n > 0)
+	{
+		nv = -1 * (n % 10);
+		_putchar(nv + '0');
+		return (nv);
+	}
+	else
+	{
+		nv = n % 10;
+		_putchar(nv + '0');
+		return (nv);
+	}
+}
+
+

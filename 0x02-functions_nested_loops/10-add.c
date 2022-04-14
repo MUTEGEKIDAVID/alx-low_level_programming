62403289a86a6d2963a06f1cbdbe3df5789eea06
MUTEGEKI DAVID <mutegeki.david555@gmail.com>
2022-04-13 18:02:42 -0700

    my commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - check main
+ * @a: An integer a
+ * @b: An integer b
+ * Description: function that adds two integers and return a result
+ * Return: result to adding a and b
+ */
+int add(int a , int b)
+{
+	return (a + b);
+}

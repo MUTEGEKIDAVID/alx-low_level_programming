00e596074057e5e506cd5b8319dab28521a8a1dc
MUTEGEKI DAVID <mutegeki.david555@gmail.com>
2022-05-31 12:17:05 -0700

    my commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/*
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename : pointer to the filename
+ * @letters:the number of letters it should read and print
+ * return  : the actual number of letters it could read and print*/
+
+
